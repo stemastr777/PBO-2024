abefd419bee07f3ee11e02565bb982246488fc6d
stemastr <stevenadi16@gmail.com>
2024-06-05 20:56:55 +0700

    Revisi tugas 2: overloading doesnt consider method return type

--- week10/BangunDatar.java
+++ week10/BangunDatar.java
@@ -3,6 +3,8 @@ package week10;
 public abstract class BangunDatar {
     
     public abstract void luas();
+
+    // public abstract int luas();  error: method luas() is already defined in class BangunDatar
     
     public abstract void keliling();
 }
